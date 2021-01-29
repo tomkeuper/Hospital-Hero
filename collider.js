9eb1cd6f29f22fe82c25dc47718d26bdb64acdff
1108188 <s1108188@student.hsleiden.nl>
2021-01-29 03:15:52 +0100

    created collider component and edited some html

--- collider.js
+++ collider.js
@@ -2,18 +2,35 @@ AFRAME.registerComponent('collide', {
     init: function (event) {
       const monkey = document.getElementById('monkey');
       const sphere = document.getElementById('js--sphere');
+      const crate = document.getElementById('js--monkeyCrate');
+      
+    
 
         this.collider = function(){
-            if(monkey.addEventListener('collide', console.log("i have collided")) && sphere.addEventListener('collide', console.log("sphere have collided"))){
-                console.log("we both collided");
-            }
-        }
-        
-    },
-    remove: function () {
-        const monkey = document.getElementById('monkey');
-        this.el.removeObject3D(monkey);
-      }
+            sphere.addEventListener('collide', (event)=>{
+                let collider = event.detail.body.el;
+                
+                if (collider.getAttribute('id') === 'js--monkey'){
+                    console.log(this);
+                    console.log('banana');
+                    crate.setAttribute('visible', 'true');
+                    let capture = document.createAttribute('animation');
+                    capture.value = 'property: position; to: 0 0 0; dur: 2000';
+                    crate.setAttribute('animation', capture.value);
 
+                    let remove = document.createAttribute('animation__remove');
+                    
+
+                } else if(collider.getAttribute('id') === 'js--penguin'){
+                    console.log('?')
+                } else {
+
+                }
+            })
+        }
+
+        this.el.addEventListener('collide', this.collider);
+        
+    }
    
   });
