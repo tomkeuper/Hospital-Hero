835487db6506c8b94af0d1d7bd412708904687f6
1108188 <s1108188@student.hsleiden.nl>
2021-02-01 23:51:07 +0100

    Updated List visuals ,added Additional monkey info

--- js/apiCall.js
+++ js/apiCall.js
@@ -1,5 +1,6 @@
 console.log('test');
 
+const back = document.getElementById('js--back');
 const textbox = document.getElementById('js--textbox');
 let BASE_URL = 'https://nl.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=apen';
 
@@ -9,5 +10,10 @@ apiCall = function(){
     .then(response => response.json())
     .then(data => textbox.innerHTML = data.query.pages['6171'].extract);
 }
+
+back.onclick = function(){
+    window.location.href = 'list.html';
+}
+
 
 apiCall();
